98b3d4290bab5150668f26cc470154095ef82178
unai-iglesias <unaeneko04@gmail.com>
2026-05-18 13:38:43 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
-
+ 
 firebase.initializeApp({
   apiKey: "AIzaSyAF4DVO6dixlCmuFzetWhJI09GShdJOxFI",
   authDomain: "organizat-ui.firebaseapp.com",
@@ -9,18 +9,16 @@ firebase.initializeApp({
   messagingSenderId: "775835679422",
   appId: "1:775835679422:web:71bc7fd3baad473e8fbcef"
 });
-
+ 
 const messaging = firebase.messaging();
-
-// Notificación recibida con la app en segundo plano o cerrada
+ 
+// Mostrar notificación cuando la app está en segundo plano o cerrada
 messaging.onBackgroundMessage(payload => {
-  const { title, body } = payload.notification;
+  const { title, body, icon } = payload.notification;
   self.registration.showNotification(title, {
     body,
-    icon: '/icono_app.png',
+    icon: icon || '/icono_app.png',
     badge: '/icono_app.png',
-    vibrate: [200, 100, 200],
-    tag: payload.data?.taskId || 'organizat',
-    renotify: true,
+    tag: payload.collapseKey || 'organizat'
   });
 });
